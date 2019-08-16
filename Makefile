fd5bd86bbe6157c2c3737e726de2a082635ac65c
FishPlusOrange <hi@fishplusorange.com>
2019-08-16 16:28:57 +0800

    Release 1.0.0

--- Makefile
+++ Makefile
@@ -1,14 +1,10 @@
 usage = "\
 Usage: make [target] \n\n\
 Available targets:\n\
-pre      打包\n\
 release  发布"
 
 default:
 	@echo $(usage)
 
-pre:
-	yarn build
-
-release:
+rel:
 	yarn release
---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -e
+echo "Enter release version:"
+read VERSION
+
+read -p "Release version $VERSION - are you sure? (y/n)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+# install
+yarn
+
+# pre
+yarn version --new-version $VERSION --no-git-tag-version
+yarn build
+
+# commit
+git tag v$VERSION
+git commit -am "Release $VERSION"
+
+# push
+git push origin master
+git push origin tags/v$VERSION
+
+# publish
+yarn publish
+fi
